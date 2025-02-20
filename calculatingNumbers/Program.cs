58652f58a0cbcdd40574c56762efe955b00730e1
LabInfo <LabInfo@TERREOA3M4>
2025-02-19 22:26:14 -0300

    Adicionar arquivos de projeto.

---
+++ calculatingNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Calculos{
+    static void Main()
+    {
+        double resultado = 0;
+
+        Console.Write("Digite um valor: ");
+        double valor1 = Convert.ToDouble(Console.ReadLine());
+        Console.Clear();
+
+        Console.Write("\nComo deseja calacular? \nSomar (+) \nSubtrair (-) \nMultiplicar (*) \nDividir (/)\n\n");
+        string calculo = Console.ReadLine();
+        Console.Clear();
+
+        Console.Write($"{valor1} {calculo} ");
+        double valor2 = Convert.ToDouble(Console.ReadLine());
+        Console.Clear();
+
+        switch (calculo)
+        {
+            case "+":
+                resultado = valor1 + valor2;
+                break;
+            case "-":
+                resultado = valor1 - valor2;
+                break;
+            case "*":
+                resultado = valor1 * valor2;
+                break;
+            case "/":
+                resultado = valor1 / valor2;
+                break;
+            default:
+                break;
+        }
+            Console.Clear();
+            Console.WriteLine("ENGRAÇADINHO!");
+    }
+}
